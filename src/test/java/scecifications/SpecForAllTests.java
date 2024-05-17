1a6ec584bbfd8c960b4e9148011fa8703500393e
Andrey <zizkun.av@list.ru>
2024-05-17 23:06:41 +0400

    Add spec

--- src/test/java/scecifications/SpecForAllTests.java
+++ src/test/java/scecifications/SpecForAllTests.java
@@ -4,12 +4,10 @@ import io.restassured.specification.RequestSpecification;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.*;
 import static io.restassured.http.ContentType.JSON;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.ResponseSpecification;
-
-import static io.restassured.filter.log.LogDetail.BODY;
-import static io.restassured.filter.log.LogDetail.STATUS;
 
 public class SpecForAllTests {
     public static RequestSpecification getrequestspecification = with()
@@ -19,7 +17,7 @@ public class SpecForAllTests {
             .log().headers()
             .contentType(JSON)
             .baseUri("https://reqres.in")
-            .basePath("/api/login");
+            .basePath("/api/user/2");
 
     public static ResponseSpecification getresponsespecification = new ResponseSpecBuilder()
             .expectStatusCode(200)
@@ -28,4 +26,61 @@ public class SpecForAllTests {
             .build();
 
 
+    public static RequestSpecification putrequestspecification = with()
+
+            .contentType(JSON)
+            .log().uri()
+            .log().headers()
+            .baseUri("https://reqres.in")
+            .basePath("/api/user/2");
+
+
+    public static ResponseSpecification putresponsespecification = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .log(STATUS)
+            .log(BODY)
+            .build();
+
+    public static RequestSpecification postrequestspecification = with()
+
+            .contentType(JSON)
+            .log().uri()
+            .log().headers()
+            .baseUri("https://reqres.in")
+            .basePath("/api/user");
+
+    public static ResponseSpecification postresponsespecification = new ResponseSpecBuilder()
+            .expectStatusCode(201)
+            .log(STATUS)
+            .log(BODY)
+            .log(HEADERS)
+            .build();
+
+    public static RequestSpecification delrequestspecification = with()
+
+            .log().uri()
+            .log().headers()
+            .baseUri("https://reqres.in")
+            .basePath("/api/user/2");
+
+    public static ResponseSpecification delresponsespecification = new ResponseSpecBuilder()
+            .expectStatusCode(204)
+            .log(STATUS)
+            .log(BODY)
+            .log(HEADERS)
+            .build();
+
+    public static RequestSpecification twopostrequestspecification = with()
+            .contentType(JSON)
+            .log().uri()
+            .log().headers()
+            .baseUri("https://reqres.in")
+            .basePath("/api/login");
+
+    public static ResponseSpecification twopostresponsespecification = new ResponseSpecBuilder()
+            .expectStatusCode(400)
+            .log(STATUS)
+            .log(BODY)
+            .log(HEADERS)
+            .build();
 }
--- src/test/java/tests/ReqresLombokTests.java
+++ src/test/java/tests/ReqresLombokTests.java
@@ -1,134 +1,128 @@
 package tests;
 
-import io.qameta.allure.restassured.AllureRestAssured;
 import models.lombok.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Type;
 
-import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static scecifications.SpecForAllTests.*;
 
 
 @DisplayName("Тесты по тестированию платформы https://reqres.in/")
 public class ReqresLombokTests {
 
-    @DisplayName("Метод Get - тест на наличие email и успешность получения верного кода ответа(код 200)")
+    @DisplayName("Метод Get - наличие email и успешность получения верного кода ответа(код 200)")
     @Test
     void checkEmailAndStatusCodePositiveTest() {
         GetResponseLombok responsetwo =
-                given()
-                        .filter(withCustomTemplates())
-                        .log().uri()
-                        .get("https://reqres.in/api/users/2")
-                        .then()
-                        .log().status()
-                        .log().headers()
-                        .log().body()
-                        .statusCode(200)
-                        .extract().as((Type) GetResponseLombok.class);
+                step("Отправка  Get запроса", () ->
+                        given(getrequestspecification)
+                                .get("")
+                                .then()
+                                .spec(getresponsespecification)
+                                .extract().as((Type) GetResponseLombok.class)
+                );
 
-        assertThat(responsetwo.getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
-        assertThat(responsetwo.getData().getFirst_name()).isEqualTo("Janet");
-        assertThat(responsetwo.getData().getLast_name()).isEqualTo("Weaver");
+        step("Проверка полученного ответа на наличие определенных данных", () -> {
+
+                    assertThat(responsetwo.getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
+                    assertThat(responsetwo.getData().getFirst_name()).isEqualTo("Janet");
+                    assertThat(responsetwo.getData().getLast_name()).isEqualTo("Weaver");
+                }
+        );
     }
 
-    @DisplayName("Метод Put - тест на update уже имеющейся сущности(name,job) и успешность получения верного кода ответа(код 200)")
+    @DisplayName("Метод Put - update уже имеющейся сущности(name,job) и успешность получения верного кода ответа(код 200)")
     @Test
     void successfulLoginTest() {
         PutRequestLombok requestLombok = new PutRequestLombok();
         requestLombok.setName("morpheus");
         requestLombok.setJob("zion resident");
 
-        PutResponseLombok response = given()
-                .body(requestLombok)
-                .contentType(JSON)
-                .log().uri()
-                .log().headers()
-                .when()
-                .put("https://reqres.in/api/users/2")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(PutResponseLombok.class);
+        PutResponseLombok response =
+                step("Отправка  Put запроса", () ->
+                        given(putrequestspecification)
+                                .body(requestLombok)
+                                .when()
+                                .put("")
+                                .then()
+                                .spec(putresponsespecification)
+                                .extract().as(PutResponseLombok.class)
+                );
 
-        assertEquals("morpheus", response.getName());
-        assertEquals("zion resident", response.getJob());
+        step("Проверка полученного ответа на наличие определенных данных - Name и Job", () -> {
+                    assertEquals("morpheus", response.getName());
+                    assertEquals("zion resident", response.getJob());
 
-        assertThat(response.getName()).isEqualTo("morpheus");
-        assertThat(response.getJob()).isEqualTo("zion resident");
+                    assertThat(response.getName()).isEqualTo("morpheus");
+                    assertThat(response.getJob()).isEqualTo("zion resident");
+                }
+        );
     }
 
-    @DisplayName("Метод Post - тест на создание нового пользователя(name,job) и успешность получения верного кода ответа(код 200)")
+    @DisplayName("Метод Post - создание нового пользователя(name,job) и успешность получения верного кода ответа(код 200)")
     @Test
     void successfulCreatedUserTest() {
         PostRequestLombok postRequestLombok = new PostRequestLombok();
         postRequestLombok.setName("morpheus");
         postRequestLombok.setJob("leader");
 
-        PostResponseLombok response = given()
-                .body(postRequestLombok)
-                .contentType(JSON)
-                .log().uri()
+        PostResponseLombok response =
+                step("Отправка  Post запроса", () ->
+                        given(postrequestspecification)
+                                .body(postRequestLombok)
+                                .when()
+                                .post("")
+                                .then()
+                                .spec(postresponsespecification)
+                                .extract().as(PostResponseLombok.class)
+                );
 
-                .when()
-                .post("https://reqres.in/api/users")
+        step("Проверка полученного ответа на наличие определенных данных - Name и Job", () -> {
 
-                .then()
-                .log().status()
-                .log().headers()
-                .log().body()
-                .statusCode(201)
-                .extract().as(PostResponseLombok.class);
-
-        assertEquals("morpheus", response.getName());
-        assertEquals("leader", response.getJob());
-
-        assertThat(response.getName()).isEqualTo("morpheus");
-        assertThat(response.getJob()).isEqualTo("leader");
+//                    assertEquals("morpheus", response.getName()); второй вариант
+//                    assertEquals("leader", response.getJob());
+                    assertThat(response.getName()).isEqualTo("morpheus");
+                    assertThat(response.getJob()).isEqualTo("leader");
+                }
+        );
     }
 
-    @DisplayName("Метод Delete - тест на удаление пользователя и успешность получения верного кода ответа(код 204)")
+    @DisplayName("Метод Delete - удаление пользователя и успешность получения верного кода ответа(код 204)")
     @Test
     void deleteUserAndStatusCodeTest() {
-        given()
-                .log().uri()
-                .delete("https://reqres.in/api/users/2")
-                .then()
-                .log().status()
-                .log().body()
-                .log().headers()
-                .statusCode(204);
+        step("Отправка  Post запроса", () ->
+                given(delrequestspecification)
+                        .delete("")
+                        .then()
+                        .spec(delresponsespecification)
+        );
     }
 
-    @DisplayName("Метод Post - тест на не валюдную авторизацию пользователя и успешность получения верного кода ответа(код 400)")
+    @DisplayName("Метод Post - не валюдную авторизацию пользователя и успешность получения верного кода ответа(код 400)")
     @Test
     void loginUserUnsuccessfulTest() {
-
         TwoPostRequestLombok twopostRequestLombok = new TwoPostRequestLombok();
         twopostRequestLombok.setEmail("peter@klaven");
 
-
-        TwoPostResponseLombok response = given()
-                .body(twopostRequestLombok)
-                .contentType(JSON)
-                .log().uri()
-
-                .when()
-                .post("https://reqres.in/api/login")
-
-                .then()
-                .log().status()
-                .log().body()
-                .log().headers()
-                .statusCode(400)
-                .extract().as(TwoPostResponseLombok.class);
-
-        assertThat(response.getError()).isEqualTo("Missing password");
+        TwoPostResponseLombok response =
+                step("Отправка  Post запроса", () ->
+                        given(twopostrequestspecification)
+                                .body(twopostRequestLombok)
+                                .when()
+                                .post("")
+                                .then()
+                                .spec(twopostresponsespecification)
+                                .extract().as(TwoPostResponseLombok.class)
+                );
+        step("Проверка полученного ответа на наличие определенного текста ошибки - Missing password", () -> {
+                    assertThat(response.getError()).isEqualTo("Missing password");
+                }
+        );
     }
 }
