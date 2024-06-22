740a616f888ec9619e8230f5a0bb86a6b8b0dbfd
Andrey <zizkun.av@list.ru>
2024-06-22 21:11:45 +0400

    Commit

--- src/test/java/tests/ReqresTests.java
+++ src/test/java/tests/ReqresTests.java
@@ -123,7 +123,7 @@ public class ReqresTests extends TestBase {
         );
     }
 
-    @DisplayName("Method Post - Check for unsuccessful user registration")
+    @DisplayName("Post method - Check for unsuccessful user registration")
     @Tags({
             @Tag("userData"),
             @Tag("all")
